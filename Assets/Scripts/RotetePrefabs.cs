c1c8eb75591e72f7c9b6e2111eb0ede1ae7eb2c2
Denis M <40519107+dmironuk@users.noreply.github.com>
2020-05-21 16:20:25 +0200

    Initial commit

---
+++ Assets/Scripts/RotetePrefabs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotetePrefabs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = UnityEngine.Random.Range(-10f, 10f);
+        float y = UnityEngine.Random.Range(1f, 5f);
+        float z = UnityEngine.Random.Range(-10f, 10f);
+        Vector3 angle = new Vector3(x, y, z);
+        transform.Rotate(new Vector3(90.0f, 0.0f, 0.0f) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/prefabScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabScript : MonoBehaviour
+{
+    public Transform myPrefab;
+    public int numberOfObjects = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float x = UnityEngine.Random.Range(-100f, 100f);
+            float y = 1;
+            float z = UnityEngine.Random.Range(-100f, 100f);
+            Vector3 position = new Vector3(x, y, z);
+            Vector3 rotationVector = new Vector3(0, 30, 0);
+            Quaternion rotation = Quaternion.Euler(rotationVector);
+            Instantiate(myPrefab, position, rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+       transform.Rotate(0, 100 * Time.deltaTime, 0, Space.Self);
+    }
+}
