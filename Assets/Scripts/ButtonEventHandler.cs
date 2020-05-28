876c5acbef2cd5bc1dda87632b2242a486251abc
Mona Lisa <40519107+dmironuk@users.noreply.github.com>
2020-05-28 19:05:26 +0200

    Changes

---
+++ Assets/Scripts/ButtonEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    void Start()
+    {
+        // Search for all Children from this ImageTarget with type VirtualButtonBehaviour
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            // Register with the virtual buttons TrackableBehaviour
+            vbs[i].RegisterEventHandler(this);
+        }
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "btn1":
+                // Do something
+                break;
+            case "btn2":
+                // Do Something
+                break;
+        }
+    }
+
+}
