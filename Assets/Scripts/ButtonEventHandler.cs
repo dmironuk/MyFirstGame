25fb223bdb37171b85bc024be9e7e57cab4c643d
Mona Lisa <40519107+dmironuk@users.noreply.github.com>
2020-05-28 20:44:36 +0200

    Virtual Buttons

--- Assets/Scripts/ButtonEventHandler.cs
+++ Assets/Scripts/ButtonEventHandler.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class ButtonEventHandler : MonoBehaviour//, IVirtualButtonEventHandler
+public class ButtonEventHandler : MonoBehaviour
 {
+    public Transform Player;
 
     void Start()
     {
@@ -13,26 +14,37 @@ public class ButtonEventHandler : MonoBehaviour//, IVirtualButtonEventHandler
         for (int i = 0; i < vbs.Length; ++i)
         {
             // Register with the virtual buttons TrackableBehaviour
-            //vbs[i].RegisterEventHandler(this);
+            vbs[i].RegisterOnButtonPressed(OnButtonPressed);
+            vbs[i].RegisterOnButtonReleased(OnButtonReleased);
         }
     }
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         switch (vb.VirtualButtonName)
         {
-            case "Backward":
-                // Do something
-                break;
             case "Forward":
+                // Do something
+                Debug.Log("Forward");
+                Player.transform.Translate(Vector3.forward * 100.0f * Time.deltaTime, Space.World);
+                break;
+            case "Backward":
                 // Do Something
+                Debug.Log("Backward");
+                Player.transform.Translate(-Vector3.forward * 100.0f * Time.deltaTime, Space.World);
                 break;
             case "Left":
                 // Do Something
+                Debug.Log("Left");
+                Player.transform.position += Vector3.left * 100.0f * Time.deltaTime;
                 break;
             case "Right":
                 // Do Something
+                Debug.Log("Right");
+                Player.transform.position += Vector3.right * 100.0f * Time.deltaTime;
                 break;
         }
     }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) { }
 
 }
