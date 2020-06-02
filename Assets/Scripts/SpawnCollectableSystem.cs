a7b70ab0bedeba818fc98efb076e2278905d2005
Mona Lisa <40519107+dmironuk@users.noreply.github.com>
2020-06-03 00:49:26 +0200

    Übung 4

---
+++ Assets/Scripts/SpawnCollectableSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class SpawnCollectableSystem : SystemBase
+{  
+    BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+    protected override void OnCreate()
+    {
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+
+    }
+
+    protected override void OnUpdate()
+    {
+        var randomArray = World.GetExistingSystem<RandomSystem>().RandomArray;
+        var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+
+
+        Entities
+            .WithNativeDisableParallelForRestriction(randomArray)
+            .ForEach((Entity entity, int nativeThreadIndex, int entityInQueryIndex, in SpawnCollectableComponent collectables) =>
+            {
+                for (int i = 1; i <= collectables.numberCollectables; i++)
+                {
+                    var random = randomArray[nativeThreadIndex];
+
+                    Entity entityInstance = commandBuffer.Instantiate(entityInQueryIndex, collectables.CollectablePrefab);
+
+                    float xPosition = random.NextFloat(-10f, 10f);
+                    float zPosition = random.NextFloat(-10f, 10f);
+
+                    float3 position = new float3(xPosition, 1f, zPosition);
+
+                    commandBuffer.AddComponent(entityInQueryIndex, entityInstance, new Translation { Value = position });
+                    commandBuffer.AddComponent(entityInQueryIndex, entityInstance, new CollectedComponent { isCollected = false, rotationAngle = 0f });
+                    randomArray[nativeThreadIndex] = random;
+                }
+
+                commandBuffer.DestroyEntity(entityInQueryIndex, entity);
+            }).ScheduleParallel(); ;
+
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
